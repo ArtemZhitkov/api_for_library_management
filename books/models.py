51079eb8f4d0bd50ccb181b35096940b8d00133c
Artem Zhitkov <aptem_deadly@mail.ru>
2025-05-16 22:25:59 +0500

    add model "Book"

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from author.models import Author
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название книги", help_text="Укажите название книги")
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="authors",
+        verbose_name="Автор",
+        help_text="Укажите автора книги",
+    )
+    genre = models.CharField(max_length=150, verbose_name="Жанр книги", help_text="Укажите жанр", **NULLABLE)
+    description = models.TextField(verbose_name="Описание книги", **NULLABLE)
+    quantity = models.PositiveSmallIntegerField(verbose_name="Количество книг")
+    publication_date = models.DateField(verbose_name="Дата публикации", **NULLABLE)
+    publisher = models.CharField(max_length=150, verbose_name="Издатель")
