35f9736e74985c0e39e3b98e88c662d37f2b6910
Artem Zhitkov <aptem_deadly@mail.ru>
2025-05-17 11:07:12 +0500

    add urls

--- books/urls.py
+++ books/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
-from books.views import BookViewSet
+from books.views import BookViewSet, GenreViewSet, CategoryViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"", BookViewSet, basename="books")
+router.register(f"genres", GenreViewSet, basename="genres")
+router.register(r"categories", CategoryViewSet, basename="categories")
 
 from books.apps import BooksConfig
 
 app_name = BooksConfig.name
 
-urlpatterns = [
-    path("", include(router.urls))
-]
+urlpatterns = [path("", include(router.urls))]
